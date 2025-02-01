1b487fc44139c28429d67147b97713f13def9413
rahilcsit <rahilcsit@gmail.com>
2025-02-02 00:21:24 +0530

    assignment solve

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,10 +15,11 @@ public class Main {
    }
     public static void main(String[] args) {
         B b1=new B ();
-//        b1.meth();
-//        System.out.println(b1.meth());
         b1.meth();
-        System.out.println(b1.meth());
+//        System.out.println(b1.meth());
+        B b2=new B();
+        b2.meth();
+//        System.out.println(b1.meth());
     }
 
 }
